974c9025080a252e3c1cb9322e9ea65a4f49a291
Angelo Dutra <Ange.utra@gmail.com>
2025-12-31 16:29:41 -0300

    Squashed 'poo/lista1/' content from commit 625cef8
    git-subtree-dir: poo/lista1
    git-subtree-split: 625cef8c1151ecac093f546e0194965bd39c769b

---
+++ List1/src/pessoa.java
@@ -0,0 +1,17 @@
+public class pessoa {
+    String name;
+    int age;
+    double height;
+
+    // NAME
+    public String getName() { return name; };
+    public void setName(String n) { this.name = n; };
+
+    // AGE
+    public int getAge(){ return age; };
+    public void setAge(int a){ this.age = a; };
+
+    // HEIGHT
+    public double getHeight() { return height; };
+    public void setHeight(double h) { this.height = h; };
+}
